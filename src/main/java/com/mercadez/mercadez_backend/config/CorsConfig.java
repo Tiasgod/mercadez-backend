28967378b0a547bfced5339191793d0770cdc3fd
Tiasgod <tigo4581@hotmail.com>
2026-01-22 11:17:31 -0300

    Configura Neon + Render

--- src/main/java/com/mercadez/mercadez_backend/config/CorsConfig.java
+++ src/main/java/com/mercadez/mercadez_backend/config/CorsConfig.java
@@ -1,16 +1,23 @@
 package com.mercadez.mercadez_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
